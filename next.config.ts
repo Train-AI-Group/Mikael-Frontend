dd77583da3b13c68e348e8543e5f25521eaa4496
Alex Bilozor <79287796+bilozorDev@users.noreply.github.com>
2024-11-21 20:34:28 -0800

    more BAD rules for PROD

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  ignoreDuringBuilds: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
 
-};
-
-export default nextConfig;
+export default nextConfig
