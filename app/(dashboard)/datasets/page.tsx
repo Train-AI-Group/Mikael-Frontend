c015adac1f1b489a81d078d2094fc480e434ec40
Aung Kaung Myat <69038584+icepcp@users.noreply.github.com>
2024-11-25 21:18:29 +0000

    :construction: add discover page function

--- app/(dashboard)/datasets/page.tsx
+++ app/(dashboard)/datasets/page.tsx
@@ -1,8 +1,36 @@
+
 import { SearchDatasets } from "@/features/SearchDatasets/SearchDatasets"
 import { DatasetsHeading } from "@/components/DatasetsHeading/DatasetsHeading"
 import { Dataset } from "@/components/Dataset/Dataset"
 
-export default function Dashboard() {
+async function fetchDatasets() {
+  //TODO: work on making the 
+  // let data = await fetch("http://localhost:3000/auth/fetchAllDataset", {
+  //   method: "POST",
+  //   body: JSON.stringify({
+  //     "transactionIds": [
+  //       "uElDx6rnRun9ZgxrxhE6DvNFNzY7zZeRMB1me8xSuHU"
+  //     ],
+  //   }),
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // });
+  //using json as it's simpler for me to move between pcs
+  let data = await fetch("http://localhost/response.json");
+  let response = await data.json();
+
+  response.transactions[0].tags.forEach(tag => {
+    tag.name = atob(tag.name);
+    tag.value = atob(tag.value);
+  });
+
+  console.log("hi", response.transactions[0].tags[7].value);
+  return response;
+}
+
+export default async function Dashboard() {
+  const datasets = await fetchDatasets();
   return (
     <>
       <DatasetsHeading
@@ -10,56 +38,18 @@ export default function Dashboard() {
         description="Explore and utilize a diverse range of machine learning models, from advanced diffusion models to cutting-edge large language models (LLMs), tailored to meet various needs and applications."
       />
       <SearchDatasets />
-      <Dataset
-        title="Dataset 1"
-        authorName="Author 1"
-        updatedTimeAgo="1 day ago"
-        authorProfileUrl="https://example.com"
-        likesCount={100}
-        usability={7.5}
-        numberOfFiles={1}
-        weightOfFiles={100}
-      />
-      <Dataset
-        title="Dataset 1"
-        authorName="Author 1"
-        updatedTimeAgo="1 day ago"
-        authorProfileUrl="https://example.com"
-        likesCount={100}
-        usability={7.5}
-        numberOfFiles={1}
-        weightOfFiles={100}
-      />
-      <Dataset
-        title="Dataset 1"
-        authorName="Author 1"
-        updatedTimeAgo="1 day ago"
-        authorProfileUrl="https://example.com"
-        likesCount={100}
-        usability={7.5}
-        numberOfFiles={1}
-        weightOfFiles={100}
-      />
-      <Dataset
-        title="Dataset 1"
-        authorName="Author 1"
-        updatedTimeAgo="1 day ago"
-        authorProfileUrl="https://example.com"
-        likesCount={100}
-        usability={7.5}
-        numberOfFiles={1}
-        weightOfFiles={100}
-      />
-      <Dataset
-        title="Dataset 1"
-        authorName="Author 1"
-        updatedTimeAgo="1 day ago"
-        authorProfileUrl="https://example.com"
-        likesCount={100}
-        usability={7.5}
-        numberOfFiles={1}
-        weightOfFiles={100}
-      />
+      {datasets.transactions.map((dataset) => (
+        <Dataset
+          title={'Dataset 1'}
+          authorName={'john'}
+          updatedTimeAgo={dataset.tags[0].updatedTimeAgo}
+          authorProfileUrl={dataset.tags[0].authorProfileUrl}
+          likesCount={dataset.tags[0].likesCount}
+          usability={1}
+          numberOfFiles={1}
+          weightOfFiles={dataset.data_size}
+        />
+      ))}
     </>
-  )
+  );
 }
