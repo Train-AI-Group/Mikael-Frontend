fdea6a844a1c6368d86f2a54c460b89048d3d8e6
dfirshteyn <dfirshteyn@gmail.com>
2024-11-26 00:10:26 -0600

    Added FAQs, added Settings page, Added Help page, Fixed 404s

--- components/HomepageHeader/HomepageHeader.tsx
+++ components/HomepageHeader/HomepageHeader.tsx
@@ -21,7 +21,7 @@ export const HomepageHeader = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex-shrink-0">
-            <Link href="/">
+            <Link href="/home">
               <Image
                 src="/img/logo.svg"
                 alt="Logo"
